a9cafdac778d9267ec61f6b7479510b9239c1d0a
Jérôme Avoustin <jerome@rehia.fr>
2015-11-13 17:08:06 +0100

    chore: add scripts and data to simulate data in demo mode

---
+++ inject.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var _ = require('lodash');
+var config = require('./config');
+var url = require('url');
+var request = require('request');
+var items = require('./data.json');
+
+var urlParam = {
+    protocol: 'http',
+    hostname: config.public.hostname,
+    query: {
+        api_key: config.api.key
+    }
+};
+
+function getApiUrl(obj) {
+    return url.format(_.extend(obj, urlParam));
+}
+
+var global_count;
+iterateOverItems();
+
+function iterateOverItems() {
+    var count = 0;
+    global_count++;
+    items.forEach(function (item) {
+        item = {
+            data: item.data,
+            geojson: item.geojson,
+            id: item.id.split('-')[2] + global_count.toString()
+        };
+        setTimeout(function () {
+            request({
+                method: 'POST',
+                uri: getApiUrl({
+                    pathname: 'api/items/item'
+                }),
+                json: { item: item }
+            }, function(error, res) {
+                if (error) {
+                    console.log('error for data id ' + item.id);
+                    console.log(error);
+                }
+            });
+            count--;
+            if (count === 0) {
+                console.log('restart');
+                iterateOverItems();
+            }
+        }, (count + 1) * 200);
+        count++;
+    });
+}
